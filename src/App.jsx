bb0c300ba5fb1b57d81f80132c7f1669e3bd09cd
Usama <usama.hafeez@gmail.com>
2022-12-31 12:27:27 +0500

    Basic board set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useReducer, useState } from 'react'
+import Board from "./components/Board"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const TYPE_MOVE = 'move'
+
+  const initialBoard = [[null, null, null],[null, null, null],[null, null, null]];
+  const initialState = {
+    board: initialBoard,
+    nextPlayer: 'X',
+    winner: null,
+  }
+
+  const tttReducer = (state, action) => {
+
+    switch (action.type) {
+      case TYPE_MOVE:
+
+        const { y, x } = action.payload
+        let board = JSON.parse(JSON.stringify(state.board))
+
+        if (board[y][x] !== null) {
+          return state
+        }
+
+        board[y][x] = state.nextPlayer
+        const nextPlayer = state.nextPlayer === 'X' ? 'O' : 'X'
+
+        return {
+          ...state,
+          board,
+          nextPlayer,
+        }
+
+      default:
+        return state;
+    }
+  }
+  
+  const [state, dispatch] = useReducer(tttReducer, initialState)
+  const { board, nextPlayer } = state
+
+  const handleMove = (y, x) => {
+    dispatch({
+      type: TYPE_MOVE,
+      payload: {
+        y,
+        x,
+      }
+    })
+    
+  }
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <main>
+        <h1>Tic tac toe</h1>
+        <Board board={board} onMove={handleMove} />
+      </main>
+    </>
   )
 }
 
---
+++ src/components/Board.jsx
@@ -0,0 +1,16 @@
+const Board = ({ board, onMove }) => {
+
+    const handleBoxClick = (e, x, y) => {
+        onMove(y,x)
+    }
+
+    return <>
+    <div className="board">
+        {[0,1,2].map(y => <div key={'y' + y} className="row">
+            {[0,1,2].map(x => <div key={'x' + x} className="col" onClick={e => {handleBoxClick(e, x, y)}}>{ board[y][x] }</div>)}
+        </div>)}
+    </div>
+    </>
+}
+
+export default Board
